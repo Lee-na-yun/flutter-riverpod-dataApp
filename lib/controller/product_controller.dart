d985dbccf4d86720f0723cdaccd811b22f000fee
Lee-na-yun <happyin3333@nate.com>
2022-11-15 17:53:05 +0900

    코드 수정 필요

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -4,21 +4,21 @@ import 'package:riverpod_dataapp/domain/product/product.dart';
 import 'package:riverpod_dataapp/domain/product/product_http_repository.dart';
 
 final productController = Provider((ref) {
-  return ProductController(ref);
+  return ProductController();
 });
 
-/*class ProductController {
-  ProductHttpRepository repo = ProductHttpRepository();*/
 class ProductController {
+  ProductHttpRepository repo = ProductHttpRepository();
+/*class ProductController {
   final Ref _ref;
-  ProductController(this._ref);
+  ProductController(this._ref);*/
 
-  /*void findAll() {
+  void findAll() {
     List<Product> productList = repo
         .findAll(); // 이 데이터는 provider의 창고(store)에 주기 //view는 controller에 요청만 함
-  }*/
-  void findAll() {
+  }
+  /*void findAll() {
     List<Product> productList = _ref.read(productHttpRepository).findAll();
     _ref.read(productListViewModel.notifier).onLoad(productList);
-  }
+  }*/
 }
